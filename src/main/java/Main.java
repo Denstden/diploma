d480025177af65c4ecdeb266bca8d33048d5f301
Denst <denstden@gmail.com>
2016-06-22 23:27:22 +0300

    edited program arguments handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,37 +4,34 @@ import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import util.FolderCleaner;
 import generator.Generator;
 import parser.Parser;
 import parser.config.test.TestConfigData;
 import test.Test;
+import util.FolderCleaner;
 
 public class Main {
     /**PROGRAM ARGUMENTS
-     *<mod> <config_file>
-     *
-     *<config_file>::=<DataSource>
-     *                <ResultSource>
-     *                <CountVariants>
-     *                <CountLinesOfVariantPreambula>
-     *                <VariantPreambula>
-     *           if mod==1
-     *                <CountQuestionsType1>
-     *                <CountQuestionsType2>
-     *                <CountQuestionsType3>
-     *                <CountQuestionsType4>
+     * <mod> <path_to_config_xml>
+     *     if (mod == 2)
+     *          <path_to_result_folder>
      */
     public static void main(String[] args) {
         Parser parser = new Parser();
+        if (args.length < 2){
+            System.err.println("Program should has at least 2 program arguments.");
+            return;
+        }
         try {
-            TestConfigData testConfigData = parser.parse("config.xml");
+            TestConfigData testConfigData = parser.parse(args[1]);
             Generator generator = new Generator();
             generator.setTestConfigData(testConfigData);
             Test test = generator.generate();
             System.out.println(test.toString());
-            FolderCleaner.deleteAllFilesFolder("newFolder");
-            test.toFile("newFolder");
+            if (Integer.valueOf(args[0]) == 1 && args.length >= 3) {
+                FolderCleaner.deleteAllFilesFolder(args[2]);
+                test.toFile(args[2]);
+            }
         } catch (IOException | SAXException | ParserConfigurationException e) {
             e.printStackTrace();
         }
