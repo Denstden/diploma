fda1a9f7700b3d7d7ae384940cd159a18d317ed2
Denst <denstden@gmail.com>
2016-06-23 11:52:17 +0300

    remove one of command line parameters(it is in config.xml)

--- src/main/java/ua/kiev/unicyb/Main.java
+++ src/main/java/ua/kiev/unicyb/Main.java
@@ -38,11 +38,12 @@ public class Main {
 			generator.setTestConfigData(testConfigData);
 			Test test = generator.generate();
 			System.out.println(test.toString());
-			if (Integer.valueOf(args[0]) == 2 && args.length >= 3) {
-				FolderCleaner.deleteAllFilesFolder(args[2]);
-				test.toFile(args[2]);
-				if (args.length == 4) {
-					test.printCorrectAnswersToFile(args[3]);
+			if (Integer.valueOf(args[0]) == 2 && args.length >= 2) {
+				String sourceFolderPath = testConfigData.getTestConfig().getResultSource();
+				FolderCleaner.deleteAllFilesFolder(sourceFolderPath);
+				test.toFile(sourceFolderPath);
+				if (args.length == 3) {
+					test.printCorrectAnswersToFile(args[2]);
 				}
 			}
 		} catch (IOException | SAXException | ParserConfigurationException | EmptyFolderException | DeleteFileException
